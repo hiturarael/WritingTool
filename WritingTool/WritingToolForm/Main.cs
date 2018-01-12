a2ac3387ece328d81b87aaada446a3200425cc6c
hiturarael <hiturarael@hotmail.com>
2018-01-12 13:56:04 -0500

    Base form added

---
+++ WritingTool/WritingToolForm/Main.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WritingToolForm
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+            lblVrsnNum.Text = Application.ProductVersion;
+        }
+    }
+}
