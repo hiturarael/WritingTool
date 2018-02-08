5754b63ecc282dc74ee77b4d89833726e47967b3
hiturarael <hiturarael@hotmail.com>
2018-02-07 22:37:19 -0500

    comments

--- WritingTool/WritingToolForm/Main.cs
+++ WritingTool/WritingToolForm/Main.cs
@@ -13,49 +13,59 @@ namespace Novelis
 {
     public partial class Main : Form
     {
-
+        #region Main Form
+        /// <summary>
+        /// Initializes the main form 
+        /// </summary>
         public Main()
         {
             InitializeComponent();
         }
 
+        /// <summary>
+        /// Handles the load action of the form
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void Main_Load(object sender, EventArgs e)
         {
+            //write the version number
             lblVrsnNum.Text = Application.ProductVersion;
 
+            //writes the project titles to the select box
             WriteProjectLoadDisplay();
         }
 
+        #endregion
+
+        #region  Clicks
+        /// <summary>
+        /// Handles functionality for the New Project button
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void BtnNewPrj_Click(object sender, EventArgs e)
         {
+            //instantiate new project
             Project newProject = new Project();
 
+            //create a new form and display
             NewForms.ProjectForm newForm = new NewForms.ProjectForm(newProject, true);
-
             newForm.ShowDialog();
 
+            //update the project display panel
             WriteProjectLoadDisplay();
 
+            //create the menu and display
             ProjectMenu.ProjectMenu menu = new ProjectMenu.ProjectMenu(newProject);
             menu.ShowDialog();
-
-            WriteProjectLoadDisplay();
         }
 
-        private void WriteProjectLoadDisplay()
-        {
-            string filepath = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + "\\Novelis";
-            List<string> loadContents = new List<string>();
-
-            if (Directory.Exists(filepath))
-            {
-                foreach (string directory in Directory.GetDirectories(filepath))
-                { loadContents.Add(directory.Replace(filepath + "\\", "")); }
-
-                listBoxPrjs.DataSource = loadContents;
-            }
-        }
-
+        /// <summary>
+        /// Handles functionality for the Load Project button
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void BtnLoadPrj_Click(object sender, EventArgs e)
         {
             //declare variables
@@ -73,16 +83,59 @@ namespace Novelis
             prj.SetTitle(projectName);
             Project.GetCoverXML(filepath, prj);
 
+            //display the menu
             ProjectMenu.ProjectMenu menu = new ProjectMenu.ProjectMenu(prj);
             menu.ShowDialog();
         }
 
+        /// <summary>
+        /// Handles functionality for the Delete Project button
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void BtnDelProj_Click(object sender, EventArgs e)
         {
+            //get the project name
             string projectName;
             projectName = listBoxPrjs.SelectedItem.ToString();
 
+            //Delete the project
             Project.DeleteProject(projectName);
         }
+
+        #endregion
+
+        #region Functions
+        /// <summary>
+        /// Writes the Projects to the load list.
+        /// </summary>
+        private void WriteProjectLoadDisplay()
+        {
+            //instantiate variables
+            string filepath = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + "\\Novelis";
+            List<string> loadContents = new List<string>();
+
+            //go to the directory 
+            if (Directory.Exists(filepath))
+            {
+                //if it exists then loop through each directory and add the project name
+                foreach (string directory in Directory.GetDirectories(filepath))
+                { loadContents.Add(directory.Replace(filepath + "\\", "")); }
+
+                listBoxPrjs.Enabled = true;
+            }
+
+            if(!Directory.Exists(filepath) || loadContents.Count() == 0)
+            {
+                //directory does not exist or no projects in directory then display no projects and disable the box.
+                loadContents.Add("No Projects Available");
+                listBoxPrjs.Enabled = false;
+            }
+
+            //set the data contents
+            listBoxPrjs.DataSource = loadContents;
+        }
+        #endregion
+
     }
 }
